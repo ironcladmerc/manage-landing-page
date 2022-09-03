de5ea6e4735e56e6d4b399c5ace1cefbb50fb881
Mike Harrison <mikeha_ca@yahoo.com>
2022-09-03 14:39:30 -0600

    completed project

--- qr-code-component/pages/index.tsx
+++ qr-code-component/pages/index.tsx
@@ -5,10 +5,10 @@ import QrCode from "../components/QrCode";
 
 const Home: NextPage = () => {
   return (
-    <div className="flex min-h-screen flex-col items-center justify-center py-2">
+    <div className="flex min-h-screen flex-col items-center justify-center bg-supLightGray">
       <Head>
         <title>QR Code Component</title>
-        <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" href="/favicon-32x32.png" />
       </Head>
       <QrCode />
     </div>
